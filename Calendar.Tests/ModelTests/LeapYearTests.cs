57fbe392e557e0c14f76f880fe0c4ba6413580b5
bodwood <bodiewood@hotmail.com>
2022-12-05 16:30:31 -0800

    add tests to determine if a year is divisible by 4

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -9,7 +9,32 @@ namespace Calendar.Tests
     [TestMethod] //tells the complier that the following code block is an individual test that MSTest will run
     public void IsLeapYear_NumberDivisibleByFour_True() //naming criteria NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
     {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012)); //first arguement = what we expect. second is the test itself
+    }
 
+    //new [TestMethod] must be added for each test
+    [TestMethod]
+    public void IsLeapYear_NotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(2011));
+    }
+
+    //new [TestMethod] must be added for each test
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleBy100_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+
+    //new [TestMethod] must be added for each test
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleBy400_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2400));
     }
   }
 }
--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -1,10 +1,20 @@
-namespace Calendar
+namespace Calendar  //Logic goes here
 {
   public class LeapYear
   {
     public bool IsLeapYear(int year)
     {
-      return false;
+      bool leapYear = false;
+      if(year % 400 == 0){
+        leapYear = true;
+      }
+      else if(year % 100 == 0){
+        leapYear = false;
+      } else if(year % 4 == 0){
+        leapYear = true;
+      }
+      return leapYear;
+
     }
   }
 }
